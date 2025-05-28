064c463522162f12f262ab7e94a30eaba7427c32
Julio rodriguez <jcro.chris@gmail.com>
2025-05-28 01:13:02 -0600

    Actualizacion del servicio del modulo casas

--- src/app/services/casas.service.ts
+++ src/app/services/casas.service.ts
@@ -1,18 +1,36 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { API_ENDPOINTS } from 'src/config/api-endpoints';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CasasService {
+export class CasaService {
 
   private baseUrl = API_ENDPOINTS.casas;
 
   constructor(private http: HttpClient) {}
 
-  getCasas(): Observable<any[]> {
-    return this.http.get<any[]>(this.baseUrl.list);
+  getCasas(): Observable<any> {
+    return this.http.get<any>(this.baseUrl.list);
+  }
+
+  getCasa(id: number): Observable<any> {
+    return this.http.get<any>(this.baseUrl.getById(id));
+  }
+
+  createCasa(data: any): Observable<any> {
+    return this.http.post<any>(this.baseUrl.create, data);
+  }
+
+  updateCasa(data: any): Observable<any> {
+    return this.http.patch<any>(`${this.baseUrl.update}`, data);
+  }
+
+  deleteCasa(id: number): Observable<any> {
+    console.log('[DEBUG] Endpoint DELETE:', this.baseUrl.delete(id));
+    return this.http.delete<any>(this.baseUrl.delete(id));
   }
 }
+
